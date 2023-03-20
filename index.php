fdf164188fb2d1dbb941328975756a04367bac2d
michalfila02 <116151903+michalfila02@users.noreply.github.com>
2023-03-20 20:37:43 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Hello world!</title>
+</head>
+<body>
+<h3> Hello World! </h3>
+<div>
+	<p>
+		<button type="button">Useless</button>
+	</p>
+</div>
+</body>
+</html>
